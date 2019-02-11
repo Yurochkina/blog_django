292441f514a21fcbd513e105e48c9b749b70c0cb
Yurochkina <olga.yurochkina@gmail.com>
2019-02-11 12:06:32 -0800

    Added updated date on posts list and added post publish success message

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    updated_date = models.DateTimeField(default=timezone.now)
     
     def publish(self):
         self.published_date = timezone.now()
